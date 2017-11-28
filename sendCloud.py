e7d868328d21cc1d778998a607a7b53a63c570ce
Unknown <thomas.falcone06@gmail.com>
2017-11-28 12:13:18 +0100

    n

--- sendCloud.py
+++ sendCloud.py
@@ -1,20 +1,22 @@
 import http.client, urllib
+import sys
 import time
 sleep = 20 # how many seconds to sleep between posts to the channel
 key = 'Y0D6SD5C247DXZIW'  # Thingspeak channel to update
 
-for i in range(10):
-	params = urllib.parse.urlencode({'field1': i, 'field2': i+1 , 'key':key }) 
-	headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-	conn = http.client.HTTPConnection("api.thingspeak.com:80")
-	try:
-		conn.request("POST", "/update", params, headers)
-		response = conn.getresponse()
-		print(i)
-		print(response.status)
-		print(response.reason)
-		data = response.read()
-		conn.close()
-	except:
-		print("connection failed")
-	time.sleep(sleep) 
+print(sys.argv)
+#for i in range(10):
+#	params = urllib.parse.urlencode({'field1': i, 'field2': i+1 , 'key':key }) 
+#	headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+#	conn = http.client.HTTPConnection("api.thingspeak.com:80")
+#	try:
+#		conn.request("POST", "/update", params, headers)
+#		response = conn.getresponse()
+#		print(i)
+#		print(response.status)
+#		print(response.reason)
+#		data = response.read()
+#		conn.close()
+#	except:
+#		print("connection failed")
+#	time.sleep(sleep) 
