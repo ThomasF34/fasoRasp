1689b1f84e06f21ef36c1c91ee1f11b7315b26e6
Unknown <thomas.falcone06@gmail.com>
2017-11-28 11:50:40 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ sendCloud.py
@@ -0,0 +1,19 @@
+import httplib, urllib
+import time
+sleep = 20 # how many seconds to sleep between posts to the channel
+key = 'Y0D6SD5C247DXZIW'  # Thingspeak channel to update
+
+for i in range(10):
+	params = urllib.urlencode({'field1': temp, 'field2': temp+1 , 'key':key }) 
+	headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+	conn = httplib.HTTPConnection("api.thingspeak.com:80")
+	try:
+		conn.request("POST", "/update", params, headers)
+		response = conn.getresponse()
+		print temp
+		print response.status, response.reason
+		data = response.read()
+		conn.close()
+	except:
+		print "connection failed"
+	time.sleep(sleep) 
