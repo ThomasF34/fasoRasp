9cca042e8bdc63ab580429d43769ad6acccdfbf9
Unknown <thomas.falcone06@gmail.com>
2017-11-28 11:59:05 +0100

    m

--- sendCloud.py
+++ sendCloud.py
@@ -4,9 +4,9 @@ sleep = 20 # how many seconds to sleep between posts to the channel
 key = 'Y0D6SD5C247DXZIW'  # Thingspeak channel to update
 
 for i in range(10):
-	params = urllib.urlencode({'field1': i, 'field2': i+1 , 'key':key }) 
+	params = urllib.parse.urlencode({'field1': i, 'field2': i+1 , 'key':key }) 
 	headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-	conn = httplib.HTTPConnection("api.thingspeak.com:80")
+	conn = http.client.HTTPConnection("api.thingspeak.com:80")
 	try:
 		conn.request("POST", "/update", params, headers)
 		response = conn.getresponse()
