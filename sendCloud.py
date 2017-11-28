8836907f4b0fa7db0978519babab75c1a6719e3a
Unknown <thomas.falcone06@gmail.com>
2017-11-28 12:31:31 +0100

    p

--- sendCloud.py
+++ sendCloud.py
@@ -5,27 +5,18 @@ sleep = 20 # how many seconds to sleep between posts to the channel
 key = 'Y0D6SD5C247DXZIW'  # Thingspeak channel to update
 
 args = sys.argv
-
-for i in range(1,len(args)) :
-	try :
-		value = int(args[i])
-		print(value)
-	except ValueError : 
-		print("Tu me donne du texte con" + args[i])
 	
-	
-#for i in range(10):
-#	params = urllib.parse.urlencode({'field1': i, 'field2': i+1 , 'key':key }) 
-#	headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-#	conn = http.client.HTTPConnection("api.thingspeak.com:80")
-#	try:
-#		conn.request("POST", "/update", params, headers)
-#		response = conn.getresponse()
-#		print(i)
-#		print(response.status)
-#		print(response.reason)
-#		data = response.read()
-#		conn.close()
-#	except:
-#		print("connection failed")
-#	time.sleep(sleep) 
+try : 
+	params = urllib.parse.urlencode({'field1': int(args[1]), 'field2': int(args[2]) , 'key':key }) 
+	headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+	conn = http.client.HTTPConnection("api.thingspeak.com:80")
+	conn.request("POST", "/update", params, headers)
+	response = conn.getresponse()
+	print(response.status)
+	print(response.reason)
+	data = response.read()
+	conn.close()
+except ValueError : 
+	print("Attention le ième argument n'est pas un entier")
+except :
+	print("Error in sending")
