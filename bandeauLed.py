a322b0d2ca1c7ea4e130433038ce2a118fd934b5
ThomasF34 <thomas.falcone06@gmail.com>
2017-12-12 17:49:23 +0000

    changement

--- bandeauLed.py
+++ bandeauLed.py
@@ -1,7 +1,7 @@
 import time
-from grovepi import * 
+from grovepi import *
 
-pin = 3 
+pin = 3
 
 pinMode(pin,"OUTPUT")
 time.sleep(1)
@@ -9,15 +9,11 @@ i = 0
 
 analogWrite(pin,0)
 time.sleep(3)
-stop = True
-while stop:
-    for i in range(0,256,5) : 
-		try : 
-			analogWrite(pin,i)
-			time.sleep(0.02)
-		except KeyboardInterrupt :
-			stop = False
-		except : 
-			print("Erreur")
-
-		
+for i in range(0,256,5) :
+	try:
+		analogWrite(pin,i)
+		time.sleep(0.02)
+	except KeyboardInterrupt :
+		stop = False
+	except :
+		print("Erreur")
